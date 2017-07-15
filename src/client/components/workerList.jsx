553dad3033cfa44dc066ae180a4045d617de8466
Jeremy Odell <jeremy0dell@yahoo.com>
2017-07-15 15:58:37 -0500

    no

--- src/client/components/workerList.jsx
+++ src/client/components/workerList.jsx
@@ -4,7 +4,7 @@ import Worker from './worker.jsx'
 const WorkerList = ({ workers, onWorkerClick }) => (
   <ul>
     {workers.map((worker, i) => (
-      <Worker key={i} {...worker} onClick={() => onWorkerClick(worker.id)} />
+      <Worker key={i} {...worker} onClick={() => console.log(worker)} />
     ))}
   </ul>
 )
