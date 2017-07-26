0a740cd17d2a7af763a8ca894c8b23ba2fbd324e
Nate Green <nate.exe@gmail.com>
2017-07-26 15:19:13 -0500

    fix duplicate search

--- src/client/components/navBar.jsx
+++ src/client/components/navBar.jsx
@@ -17,14 +17,7 @@ const NavBar = ( {userType, handleLogout, setWorkers} ) =>
         </button>
         <a className="navbar-brand" href="#">Yard.ly</a>
       </div>
-
-      <Search workers={workers} setWorkers={setWorkers}/>
-
       <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul className="nav navbar-nav">
-          <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-          <li><a href="#">Link</a></li>
-        </ul>
         <Search setWorkers={setWorkers}/>
         <ul className="nav navbar-nav navbar-right">
           {userType === 'placeholder' ? "" :
@@ -33,17 +26,6 @@ const NavBar = ( {userType, handleLogout, setWorkers} ) =>
           : <li><a href="#" data-toggle="modal" data-target="#sign-in-modal">Sign In</a></li>
 
           }
-          <li className="dropdown">
-            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Route test <span className="caret"></span></a>
-            <ul className="dropdown-menu">
-              <li><Link to="/">/</Link></li>
-              <li><Link to="/workers">Workers</Link></li>
-              <li><Link to="/profile">Profile</Link></li>
-              <li><Link to="/user">User</Link></li>
-              <li role="separator" className="divider"></li>
-              <li><a href="#">Placeholder</a></li>
-            </ul>
-          </li>
         </ul>
       </div>
     </div>
