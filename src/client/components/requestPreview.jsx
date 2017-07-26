e5db24fe50ec218fbda66bb43ab4a6d2139c0303
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2017-07-26 17:16:25 -0500

    puts a lot components in request preview

--- src/client/components/requestPreview.jsx
+++ src/client/components/requestPreview.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import PickaDate from './pickaDate.jsx'
+import PickaServiceEquipment from './pickaServiceEquipment.jsx'
+import PickAddress from './pickAddress.jsx'
 
 const RequestPreview = props => {
 	const equipmentItems = Object.keys(props.equipment).map(function(key) {
@@ -17,40 +20,61 @@ const RequestPreview = props => {
 	})
 
 	return (
-			<div className="card cover">
-				<div className="content text-center">
-					<div className="panel-contact-info">
-						<div className="col-md-1">
-							<br />
-							<p className="calendar">
-								{props.dt[0]}<em>{props.dt[1]}</em>
-							</p>
-						</div>
-						<div className="content">
-							<h3>{props.jobname}</h3>
-							<p>
-								Worker: {props.request.workerFirst} Homeowner:{' '}
-								{props.request.userFirst}
-							</p>
-							<p>{props.date} at {props.time}</p>
-							<p>
-								{props.rate} $/hr for {props.hours} hours = ${props.hours * props.rate}
-							</p>
-						</div>
+		<div className="card cover">
+			<div className="content text-center">
+				<p className="calendar">
+					{props.dt[0]}<em>{props.dt[1]}</em>
+				</p>
+				<div className="panel-contact-info">
+					<div className="col-md-1">
+						<br />
+						<h1>Request for {props.worker.firstName}</h1>
+						<h3>{"'" + props.jobname + "'"}</h3>
+						<form onChange={props.changeJobName}>
+							<input id="requestjobname" type="text" placeholder="Jobname" />
+						</form>
+						<PickaDate
+							setDate={props.setDate}
+							setTime={props.setTime}
+							setHours={props.setHours}
+						/>
+					</div>
+					<div className="content">
+						<p>
+							Worker: {props.worker.firstName} Homeowner:{' '}
+							{props.user.firstName}
+						</p>
+						<p>{props.date} at {props.time}</p>
+						<p>
+							{props.rate} $/hr for {props.hours} hours = ${props.hours * props.rate}
+						</p>
 					</div>
 				</div>
-				<div className="text-center">
+			</div>
+			<div className="text-center">
 				<div><h3>Requested Services: </h3>{servicesItems}</div>
 				<div><h3>Requested Equipment: </h3>{equipmentItems}</div>
+				<PickaServiceEquipment
+					equipment={props.worker.equipment}
+					services={props.worker.services}
+					onServicesClick={props.onServicesClick}
+					onEquipmentClick={props.onEquipmentClick}
+				/>
 				<p>
 					Address: {props.address.address} City:{' '}
 					{props.address.city} State:{props.address.state}
 					Zip: {props.address.zipcode}
 				</p>
+				<PickAddress
+					addresses={props.user.addresses}
+					setAddress={props.setAddress}
+				/>
 				<p>{props.accepted ? 'Accepted' : 'Being Reviewed'}</p>
+				<button onClick={() => props.submitRequest()}>
+					Submit Request
+				</button>
 			</div>
-			</div>
-
+		</div>
 	)
 }
 
